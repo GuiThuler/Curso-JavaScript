8e1070b0f3234e122037c3900aad2e842ffda450
GuiThuler <guilhermevthuler@gmail.com>
2026-05-20 22:31:40 -0300

    Exercicio de FizzBuzz finalizado.

--- JavaScript/Exercicios/aula43/ex43.js
+++ JavaScript/Exercicios/aula43/ex43.js
@@ -8,33 +8,23 @@ checar se o numero é realmente um numero = retorna o proprio numero
 Use a função com os numeros de 0 a 100 *******
 */
 function eDivisiao(num) {
-    for (let i = 1; i <= 100; i++) {
-    i = num
-
-    if (num != Number) {
-        return num;
-    }
-
-    if (num % 5 === 0 && num % 3 === 0) {
+   if (typeof num !== 'number') {
+         return num;
+     }
+   if (num % 5 === 0 && num % 3 === 0) {
        return 'FizzBuzz';
     }
-
-    if (num % 3 === 0) {
+   if (num % 3 === 0) {
        return 'Fizz';
     } 
-
-    if (num % 5 === 0) {
+   if (num % 5 === 0) {
        return 'Buzz';
     }
-    
-    if (num % 3 != 0 || num % 5 != 0) {
-       return num;
-    }
+   return num;
 }
-}
+ for (let i = 1; i <= 100; i++) {
+     console.log(i , eDivisiao(i));
+ };
 
-for (let i = 1; i <= 100; i++) {
-    console.log(num = i)
-}
-
-console.log(eDivisiao(25));
+const minCod = (num) => typeof num !== 'number' ? num : num % 5 === 0 && num % 3 === 0 ? 'FizzBuzz' : num % 3 === 0 ? 'Fizz' : num % 5 === 0 ? 'Buzz' : num;
+for (let i = 1; i <= 100; i++) { console.log(i , minCod(i)); };  
