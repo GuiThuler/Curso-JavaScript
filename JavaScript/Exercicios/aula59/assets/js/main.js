aa6b6dddec11b7ecba5880e941d733ca625a8a41
GuiThuler <guilhermevthuler@gmail.com>
2026-07-03 19:29:10 -0300

    Arrumando atividade de Function construction.

--- JavaScript/Exercicios/aula59/assets/js/main.js
+++ JavaScript/Exercicios/aula59/assets/js/main.js
@@ -1,69 +1,52 @@
 function Calculadora() {
-        this.display = document.querySelector('.display');
+  this.display = document.querySelector(".display");
 
-        this.inicia = () => { // METODO
-            cliqueBotoes();
-            pressionaEnter();
-        };
+  this.inicia = () => {
+    this.cliqueBotoes();
+    this.pressionaEnter();
+  };
 
-        const pressionaEnter = () => {
-            this.display.addEventListener('keyup', e => {
-                if (e.keyCode === 13) {
-                    realizaConta();
-                }
-            })
-        };
-        
-        const realizaConta = () => {
-            let conta = this.display.value;
+  this.pressionaEnter = () => {
+    document.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13) {
+        this.realizaConta();
+      }
+    });
+  };
 
-            try {
-                conta = eval(conta);
-                if(!conta) {
-                    alert('Conta Inválida');
-                    return;
-                }
+  this.realizaConta = () => {
+    try {
+      // tenta
+      conta = eval(this.display.value);
+      if (!conta) {
+        alert("Conta Inválida");
+        return;
+      }
 
-                this.display.value = conta;
-            } catch(e) {
-                alert('Conta Inválida')
-                return;
-            }
-        };
+      this.display.value = conta;
+    } catch (e) {
+      // se nao der
+      alert("Conta Inválida");
+      return;
+    }
+  };
 
-        const apagaUm = () => {
-            this.display.value = this.display.value.slice(0, -1)
-        };
+  this.btnParaDisplay = (el) => {
+    this.display.value += el;
+    this.display.focus();
+  };
+  this.apagaUm = () => (this.display.value = this.display.value.slice(0, -1));
+  this.clearDisplay = () => (this.display.value = "");
 
-        const clearDisplay = () => {
-            this.display.value = '';
-        };
-
-        const cliqueBotoes = () =>  {
-            document.addEventListener('click', e => {
-                const el = e.target;
-
-                if(el.classList.contains('btn-num')) {
-                    btnParaDisplay(el.innerText)
-                }
-                if(el.classList.contains('btn-clear')) {
-                    clearDisplay();
-                }
-                if(el.classList.contains('btn-del')) {
-                    apagaUm();
-                }
-                if(el.classList.contains('btn-eq')) {
-                    realizaConta();
-                }
-                
-             });
-        
-        };
-
-        const btnParaDisplay = (valor) => {
-            this.display.value += valor;
-        };
-    };
-
+  this.cliqueBotoes = () => {
+    document.addEventListener("click", (e) => {
+      const el = e.target;
+      if (el.classList.contains("btn-num")) this.btnParaDisplay(el.innerText);
+      if (el.classList.contains("btn-clear")) this.clearDisplay();
+      if (el.classList.contains("btn-del")) this.apagaUm();
+      if (el.classList.contains("btn-eq")) this.realizaConta();
+    });
+  };
+}
 const calculadora = new Calculadora();
 calculadora.inicia();
