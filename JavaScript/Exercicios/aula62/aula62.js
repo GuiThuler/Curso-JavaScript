b749ce4ae68faf957b66cebaebda377e2d1ef31f
GuiThuler <guilhermevthuler@gmail.com>
2026-07-21 20:10:05 -0300

    Relembrando ARRAY.

---
+++ JavaScript/Exercicios/aula62/aula62.js
@@ -0,0 +1,11 @@
+const nomes = ['Edu' , 'Maria' , 'Joana'];
+nomes.push('João');
+const novo = nomes.slice(0, -1)
+console.log(novo);
+
+//PARA SEPARAR STRING PAPRA UM ARRAY
+
+const nick = 'Luiz Otavio Miranda';
+const nicks = nick.split(' ');
+console.log(nicks)
+
