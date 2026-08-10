c98ffb90e6fc87800c8d61ad7070b0229bfb73e7
GuiThuler <guilhermevthuler@gmail.com>
2026-08-10 20:18:48 -0300

    Praticando Filter em array.

--- JavaScript/Exercicios/aula65/aula65.js
+++ JavaScript/Exercicios/aula65/aula65.js
@@ -21,8 +21,6 @@ numMaior10()
 const numerosFiltrados = numeros.filter(valor => valor > 10);
 console.log(numerosFiltrados)
 
-// Retorne as pessoas que tem o nome com 5 letras ou mais
-
 const pessoas = [
     {nome: 'Luiz', idade: 62},
     {nome: 'Maria', idade: 23},
@@ -32,5 +30,19 @@ const pessoas = [
     {nome: 'Wallace', idade: 47}
 ]
 
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+
 const pessoasComNomeGrande = pessoas.filter(obj => obj.nome.length >= 5);
 console.log(pessoasComNomeGrande);
+
+// retorne as pessoas com mais de 50 anos
+
+const pessoasVelhas = pessoas.filter(year => year.idade >= 50);
+console.log(pessoasVelhas);
+
+//retorne as pessoas cujo o nome termina com A
+
+const nomeTerminaComA = pessoas.filter(obj => {
+    return obj.nome.toLowerCase().endsWith('a');
+});
+console.log(nomeTerminaComA);
